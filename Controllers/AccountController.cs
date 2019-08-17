47a1840a3df8ae617335cdd495114ab9873b1246
Edgar <edgar.mata.1911@gmail.com>
2019-08-17 17:07:03 -0600

    Login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -29,17 +29,24 @@ namespace Progra1_bases.Controllers
         [HttpPost]
         public IActionResult Login(string username, string password)
         {
+            //if (_context.Cliente.Any(e => e.Username == username) && _context.Cliente.Any(e => e.Password == password)) esto es mas rapido
+            //pero no se como hacer que revise en el mismo elemento si la contrasenia coincide
             var clientes = from c in _context.Cliente
                          select c;
 
             if (!String.IsNullOrEmpty(username))
             {
                 clientes = clientes.Where(s => s.Username.Equals(username) && s.Password.Equals(password));
-            }
-            if (clientes.Count() > 0)
-            {
-                HttpContext.Session.SetString("username", username);
-                return View("Success");
+                if (clientes.Count() > 0)
+                {
+                    HttpContext.Session.SetString("username", username);
+                    return View("Success");
+                }
+                else
+                {
+                    ViewBag.error = "Invalid Account";
+                    return View("Index");
+                }
             }
             else
             {
